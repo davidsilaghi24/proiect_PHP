0d684c6c52660237515f46b0b81aca0a604fbd62
davidsilaghi24 <david.silaghi24@gmail.com>
2023-12-12 00:29:58 +0200

    Adaugat mai multe view-uri

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,23 +1,35 @@
-{{-- resources/views/articles/edit.blade.php --}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Article') }}
+        </h2>
+    </x-slot>
 
-@extends('layouts.app')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <form action="{{ route('articles.update', $article) }}" method="POST" class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @csrf
+                @method('PUT')
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="mb-4">
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" value="{{ old('title', $article->title) }}" required autofocus />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
 
-@section('content')
-    <h1>Editare Articol</h1>
-    <form method="post" action="{{ route('articles.update', $article->id) }}">
-        @csrf
-        @method('PATCH')
+                    <div class="mb-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-textarea id="content" class="block mt-1 w-full" name="content" rows="10" required>{{ old('content', $article->content) }}</x-textarea>
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
 
-        <div>
-            <label for="title">Titlu</label>
-            <input type="text" name="title" id="title" value="{{ $article->title }}">
+                    <div class="flex items-center justify-end">
+                        <x-primary-button>
+                            {{ __('Update') }}
+                        </x-primary-button>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div>
-            <label for="content">Conținut</label>
-            <textarea name="content" id="content">{{ $article->content }}</textarea>
-        </div>
-
-        <button type="submit">Actualizează Articolul</button>
-    </form>
-@endsection
+    </div>
+</x-app-layout>
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,14 +1,34 @@
-{{-- resources/views/articles/index.blade.php --}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Articles Management') }}
+        </h2>
+    </x-slot>
 
-@extends('layouts.app')
-
-@section('title', 'Article List')
-
-@section('content')
-    <h1>Articles</h1>
-    <ul>
-        @foreach ($articles as $article)
-            <li>{{ $article->title }} - {{ $article->content }}</li>
-        @endforeach
-    </ul>
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <ul>
+                        @forelse ($articles as $article)
+                            <li class="mb-4 p-4 border-b last:border-b-0">
+                                <div class="flex justify-between items-center">
+                                    <div>
+                                        <a href="{{ route('articles.show', $article) }}" class="text-lg font-semibold">{{ $article->title }}</a>
+                                        <p class="text-sm text-gray-600">{{ Str::limit($article->content, 100) }}</p>
+                                    </div>
+                                    <div class="flex items-center">
+                                        <a href="{{ route('articles.edit', $article) }}" class="text-indigo-600 hover:text-indigo-900 mr-4">Edit</a>
+                                        {{-- Formularul de ștergere poate fi adăugat aici --}}
+                                    </div>
+                                </div>
+                            </li>
+                        @empty
+                            <li>{{ __('No articles found.') }}</li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
